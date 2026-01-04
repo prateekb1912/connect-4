81d3d8b5a33ed43094416a112e94d7709b92e6b9
Prateek Bhardwaj <prateekb1912@gmail.com>
2026-01-04 17:38:17 +0530

    complete with game and player states

--- main.py
+++ main.py
@@ -1,20 +1,89 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
 MAX_CONSECUTIVES = 4
 
-class Grid:
+class GameState(str, Enum):
+    IN_PROGRESS = "In Progress"
+    WON = "Won"
+    DRAW = "Draw"
+
+class DiscColor(str, Enum):
+    RED = 1
+    BLUE = 2
+
+@dataclass
+class Player:
+    color: DiscColor
+
+class Game:
+    def __init__(self, player1: "Player", player2: "Player", board: "Board"):
+        self.player1 = player1
+        self.player2 = player2
+        self.board = board
+
+        self.currentPlayer = player1
+        self.winner = None
+        self.gameState = GameState.IN_PROGRESS
+    
+    def makeMove(self, player, col):
+        if self.gameState != GameState.IN_PROGRESS:
+            return False
+
+        if player != self.currentPlayer:
+            return False
+
+        row =  self.board.dropPointer(col, player)
+
+        if row == -1:
+            return False
+
+        if self.board.checkWin(row, col, player):
+            self.gameState = GameState.WON
+            self.winner = player
+        elif self.board.isFull():
+            self.gameState = GameState.DRAW
+        else: 
+            self.currentPlayer = self.player2 if (player == self.player1) else self.player1
+
+    def getCurrentPlayer(self):
+        return self.currentPlayer
+
+    def getGameState(self):
+        return self.gameState.value
+
+    def getBoard(self):
+        return self.board
+
+class Board:
     def __init__(self, rows: int, cols: int):
         self.cells = [[0 for _ in range(cols)] for _ in range(rows)]
         self.rows = rows
         self.cols = cols
     
-    def placePointer(self, x: int, y: int, player: int):
-        self.cells[x][y] = player
+    def placePointer(self, x: int, y: int, player: Player):
+        self.cells[x][y] = player.color.value
     
-    def dropPointer(self, col: int, player: int):
-        for i in range(self.rows-1, 0, -1):
+    def dropPointer(self, col: int, player: Player):
+        if col < 0 or col >= self.cols:
+            return -1
+
+        for i in range(self.rows-1, -1, -1):
             if not self.cells[i][col]:
                 self.placePointer(i, col, player)
-                break
+                return i
+        
+        return -1
     
+    def isFull(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.cells[i][j] == 0:
+                    return False
+        
+        return True
+
     def checkWin(self, lastX: int, lastY: int, lastPlayer: int):
         consecutive = 0
         for i in range(self.rows):
@@ -71,12 +140,19 @@ class Grid:
         return grid
 
 def main():
-    grid = Grid(5, 5)
+    grid = Board(5, 5)
 
-    grid.placePointer(0, 4, 1)
-    grid.placePointer(1, 3, 1)
-    grid.placePointer(2, 2, 1)
-    grid.placePointer(3, 1, 1)
+    player1 = Player(DiscColor.BLUE)
+    player2 = Player(DiscColor.RED)
+
+    game = Game(player1, player2, grid)
+
+    game.makeMove(player1, 0)
+    game.makeMove(player2, 4)
+    game.makeMove(player2, 2)
+
+    print(game.getBoard())
+
 
 
 if __name__ == '__main__':
